d71508daf0ff0b7bb6f726d79f4fc406bd17adc0
José Sánchez-Gallego <gallegoj@uw.edu>
2019-02-07 15:41:03 -0800

    Ignore exposures with bad dscale

--- code/simuations/archive_data/simulate_guide_archive.py
+++ code/simuations/archive_data/simulate_guide_archive.py
@@ -7,7 +7,7 @@
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 #
 # @Last modified by: José Sánchez-Gallego (gallegoj@uw.edu)
-# @Last modified time: 2019-02-07 14:50:11
+# @Last modified time: 2019-02-07 15:40:44
 
 import multiprocessing
 import pathlib
@@ -76,7 +76,8 @@ def simulate(df):
     for index, gprobes in df.groupby(level=0):
 
         dra = header.loc[index].dra
-        if numpy.isnan(dra) or dra <= -999:
+        dscale = header.loc[index].dscale
+        if numpy.isnan(dra) or dra <= -999 or dscale <= -999:
             continue
 
         gprobes_filt = filter_gprobes(gprobes)
